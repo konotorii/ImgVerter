8c4aa7e9fe166b8235257cf39c5ab3470a3cdf66
Konotorii <79553360+konotorii@users.noreply.github.com>
2024-05-26 02:07:35 -0400

    Trying to get gin running

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
-package ImgVerter
+package main
+
+//import "net/http"
+//import "io"
+//import "log"
+
+//import "github.com/sunshineplan/imgconv"
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+
+	router.Run("localhost:8000")
+}
+
+func convertImage(c *gin.Context) {
+	url := c.Param("url")
 }
