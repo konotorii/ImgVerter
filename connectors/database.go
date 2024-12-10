343b92cb4e5251c4fe722b3d80420fa2fa080941
konotorii <github@konotorii.com>
2024-12-09 23:19:18 -0500

    feat: Initialize postgres on main (#22)

--- connectors/database.go
+++ connectors/database.go
@@ -12,6 +12,8 @@ var DB *gorm.DB
 func DatabaseInit() {
 	if util.Config.DatabaseDriver == "SQLITE" {
 		DB = SqliteInit()
+	} else if util.Config.DatabaseDriver == "PSQL" {
+		DB = PostgresInit()
 	}
 
 	DB.AutoMigrate(&types.DBImage{})
